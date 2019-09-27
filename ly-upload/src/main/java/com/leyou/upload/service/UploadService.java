a999932fd2736089270fc39dc86414b778725c9d
kewei <1540089431@qq.com>
2019-09-27 22:58:23 +0800

    fastDPS图片上传

--- ly-upload/src/main/java/com/leyou/upload/service/UploadService.java
+++ ly-upload/src/main/java/com/leyou/upload/service/UploadService.java
@@ -1,24 +1,34 @@
 package com.leyou.upload.service;
 
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.leyou.upload.web.UploadController;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
+@Slf4j
 public class UploadService {
 
     private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
 
     // 支持的文件类型
     private static final List<String> suffixes = Arrays.asList("image/png", "image/jpeg");
+
+    @Autowired
+    FastFileStorageClient storageClient;
+
+
 
     public String upload(MultipartFile file) {
         try {
@@ -35,22 +45,17 @@ public class UploadService {
                 logger.info("上传失败，文件内容不符合要求");
                 return null;
             }
-            // 2、保存图片
-            // 2.1、生成保存目录
-            File dir = new File("F:\\itke");
-            if (!dir.exists()) {
-                dir.mkdirs();
-            }
-            // 2.2、保存图片
-            file.transferTo(new File(dir, file.getOriginalFilename()));
 
-            // 2.3、拼接图片地址
-            String url = "http://image.leyou.com/upload/" + file.getOriginalFilename();
-
-            return url;
+            // 2、将图片上传到FastDFS
+            // 2.1、获取文件后缀名
+            String extension = StringUtils.substringAfterLast(file.getOriginalFilename(), ".");
+            // 2.2、上传
+            StorePath storePath = this.storageClient.uploadFile(
+                    file.getInputStream(), file.getSize(), extension, null);
+            // 2.3、返回完整路径
+            return "http://image.leyou.com/" + storePath.getFullPath();
         } catch (Exception e) {
             return null;
         }
     }
 }
-
