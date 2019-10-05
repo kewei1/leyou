0316f47706626fa3b419b3a06bc35cf24267da33
kewei <1540089431@qq.com>
2019-10-06 02:14:56 +0800

    test

--- leyou-search/src/main/java/com/leyou/repository/GoodsRepository.java
+++ leyou-search/src/main/java/com/leyou/repository/GoodsRepository.java
@@ -3,6 +3,6 @@ package com.leyou.repository;
 import com.leyou.search.pojo.Goods;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-//extends ElasticsearchRepository<Goods,Long>
+
 public interface GoodsRepository  extends ElasticsearchRepository<Goods,Long> {
 }
