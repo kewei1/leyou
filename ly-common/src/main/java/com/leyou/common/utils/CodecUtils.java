655366e0c84562b009e17936d31f9d1ff1eb163c
kewei <1540089431@qq.com>
2019-10-19 17:52:25 +0800

    实现短信发送功能 实现注册功能 实现根据用户名和密码查询用户功能

--- ly-common/src/main/java/com/leyou/common/utils/CodecUtils.java
+++ ly-common/src/main/java/com/leyou/common/utils/CodecUtils.java
@@ -2,12 +2,27 @@ package com.leyou.common.utils;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Random;
+
 /**
  * @Author: 98050
  * @Time: 2018-10-23 10:49
  * @Feature: 密码加密
  */
 public class CodecUtils {
+    /**
+     * 生成指定位数的随机数字
+     * @param len 随机数的位数
+     * @return 生成的随机数
+     */
+    public static String generateCode(int len){
+        len = Math.min(len, 8);
+        int min = Double.valueOf(Math.pow(10, len - 1)).intValue();
+        int num = new Random().nextInt(
+                Double.valueOf(Math.pow(10, len + 1)).intValue() - 1) + min;
+        return String.valueOf(num).substring(0,len);
+    }
+
 
     public static String passwordBcryptEncode(String username,String password){
 
