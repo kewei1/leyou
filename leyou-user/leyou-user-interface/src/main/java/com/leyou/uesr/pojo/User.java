0f225278095115d89d56706800c07f230a661964
kewei <1540089431@qq.com>
2019-10-19 15:27:24 +0800

    数据验证功能

---
+++ leyou-user/leyou-user-interface/src/main/java/com/leyou/uesr/pojo/User.java
@@ -0,0 +1,31 @@
+package com.leyou.uesr.pojo;
+
+import lombok.Data;
+import net.minidev.json.annotate.JsonIgnore;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Table(name = "tb_user")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;// 用户名
+
+    @JsonIgnore
+    private String password;// 密码
+
+    private String phone;// 电话
+
+    private Date created;// 创建时间
+
+    @JsonIgnore
+    private String salt;// 密码的盐值
+}
+
